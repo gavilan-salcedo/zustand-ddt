d7579f54f08dc4b350f1bc48b17fadb386507486
Gavilans1~ <gavilansalcedo@PSGs-MacBook-Pro.local>
2024-03-28 09:21:48 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { Typography, Input, Select, Flex, Checkbox } from 'antd';
 
 function App() {
+
+  const { Title } = Typography;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{padding: '20px', display: 'flex',flexDirection: 'column', gap: '20px'}}>
+      <Title>TODO LIST</Title>
+      <Flex gap={8}>
+        <Input placeholder="Add Todo" />
+        <Select
+          defaultValue="All"
+          style={{ width: 120 }}
+          options={[
+            { value: 'completed', label: 'Completed' },
+            { value: 'todo', label: 'To Do' },
+          ]}
+        />
+      </Flex>
+      <Flex vertical>
+        <Checkbox>Checkbox</Checkbox>
+      </Flex>
     </div>
   );
 }
